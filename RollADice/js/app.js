c3b63630ac4c21f459330438bf4d6f4f33012096
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-19 16:14:44 +0200

    Almost finished with the game

--- RollADice/js/app.js
+++ RollADice/js/app.js
@@ -7,15 +7,31 @@ let button0 = document.getElementById("button0");
 
 // TEXTS
 let playerRollText = document.getElementById("playerRollText");
+let computerRollText = document.getElementById("computerRollText");
+let evaluationRollText = document.getElementById("evaluationRollText");
+let playerScoreNumber = document.getElementById("playerScoreNumber");
+let computerScoreNumber = document.getElementById("computerScoreNumber");
+let playerScoreText = document.getElementById("playerScoreText");
+let computerScoreText = document.getElementById("computerScoreText");
+
 
 // DATA
 let playerRoll = 0;
+let computerRoll = 0;
+let evaluationResult = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 
 // PROCESSES push the button
 button0.addEventListener("click", function() {
   getRandomIntForPlayer();
   showPlayerRollResult();
+  getRandomIntForComputer();
+  showComputerRollResult();
+  evaluationResultBool();
+  showEvaluationResult();
+  showScoreResult();
 })
 
 // CONTROLLERS functions. +1 för att få bort nollan
@@ -23,7 +39,40 @@ function getRandomIntForPlayer() {
   playerRoll = Math.floor(Math.random() * 6) + 1;
 }
 
+function getRandomIntForComputer() {
+  computerRoll = Math.floor(Math.random() * 6) + 1;
+}
+
+function evaluationResultBool () {
+  if (playerRoll > computerRoll) {
+    playerScore++;
+    return "You win!";
+  }
+  if (playerRoll < computerRoll) {
+    computerScore++;
+    return "You lose!";
+  }
+  if (playerRoll === computerRoll) {
+  return "It´s a draw!";
+  }
+}
+
+
+
 // VIEWS
 function showPlayerRollResult() {
   playerRollText.innerHTML = playerRoll
+}
+
+function showComputerRollResult() {
+  computerRollText.innerHTML = computerRoll
+}
+
+function showEvaluationResult() {
+  evaluationRollText.innerHTML = evaluationResultBool();
+}
+
+function showScoreResult () {
+  playerScoreNumber.innerHTML = playerScore;
+  computerScoreNumber.innerHTML = computerScore;
 }
